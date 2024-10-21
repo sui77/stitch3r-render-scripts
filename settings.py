b5c1b6400fb15ebbadefd020650b52491ced984c
Suat Özgür <s.oezguer@gmail.com>
2024-10-21 12:52:31 +0200

    Update settings.py

--- settings.py
+++ settings.py
@@ -1,1 +1,35 @@
+import bpy
+import os
 
+# ======== output settings =============
+# bpy.data.scenes["Scene"].render.resolution_x=1204
+# bpy.data.scenes["Scene"].render.resolution_y=1024
+
+# bpy.data.scenes["Scene"].frame_start=0
+# bpy.data.scenes["Scene"].frame_end=441
+
+bpy.data.scenes["Scene"].render.filepath=""
+# bpy.data.scenes["Scene"].render.image_settings.file_format="FFMPEG"
+
+# ======== render settings =============
+# bpy.data.scenes["Scene"].render.engine="CYCLES"
+# bpy.data.scenes["Scene"].cycles.device="GPU"
+
+# bpy.data.scenes["Scene"].cycles.use_adaptive_sampling=1
+# bpy.data.scenes["Scene"].cycles.adaptive_threshold=0.1
+# bpy.data.scenes["Scene"].cycles.use_denoising=1
+# bpy.data.scenes["Scene"].cycles.samples=300
+
+# bpy.data.scenes["Scene"].cycles.max_bounces=2
+# bpy.data.scenes["Scene"].cycles.diffuse_bounces=1
+# bpy.data.scenes["Scene"].cycles.glossy_bounces=2
+# bpy.data.scenes["Scene"].cycles.transmission_bounces=1
+# bpy.data.scenes["Scene"].cycles.volume_bounces=0
+# bpy.data.scenes["Scene"].cycles.transparent_max_bounces=1
+
+# bpy.data.scenes["Scene"].cycles.caustics_reflective=0
+# bpy.data.scenes["Scene"].cycles.caustics_refractive=0
+
+# bpy.data.scenes["Scene"].cycles.use_fast_gi=1
+
+# bpy.data.scenes["Scene"].render.use_persistent_data=1
